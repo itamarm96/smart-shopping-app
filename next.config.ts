be4bd9bfd7f1e3c4181ac292781e20319167bf0c
itamar <itamark86@gmail.com>
2026-02-27 08:45:23 +0200

    Disable caching for sw.js in next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.115"],
+  async headers() {
+    return [
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Cache-Control", // Prevent caching of the service worker
+            value: "public, max-age=0, s-maxage=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
